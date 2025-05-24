c81fbac7127b6a3c2a0d39f51232076b8737a426
buaahsh <buaahsh@foxmail.com>
2025-05-24 02:44:53 -0700

    update

--- vllm/model_executor/layers/fused_moe/layer.py
+++ vllm/model_executor/layers/fused_moe/layer.py
@@ -205,6 +205,8 @@ class FusedMoE(torch.nn.Module):
         self.topk_group = topk_group
         self.custom_routing_function = custom_routing_function
 
+        print("quant_config", quant_config)
+        assert 0
         if quant_config is None:
             self.quant_method: Optional[QuantizeMethodBase] = (
                 UnquantizedFusedMoEMethod())
