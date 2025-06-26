dcb865b51ae21d11c9ccfda09d3dbdf49ae615d0
yushuiwx <wuxunyushui@gmail.com>
2025-06-26 03:16:54 -0700

    update

--- vllm/model_executor/models/bitnet.py
+++ vllm/model_executor/models/bitnet.py
@@ -527,6 +527,7 @@ class BitNetModel(nn.Module):
                 param = params_dict[name]
                 weight_loader = getattr(param, "weight_loader",
                                         default_weight_loader)
+                print("===> name", name)
                 weight_loader(param, loaded_weight)
             loaded_params.add(name)
         return loaded_params
